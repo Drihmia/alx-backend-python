a4f50759bd83e226343401e8d285beb75cb62361
Drihmia <drihmia.redouane@gmail.com>
2024-06-06 22:33:58 +0100

    test

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This module contains safe_first_element as  function"""
-from typing import Sequence, Any
+from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Any:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """safe_first_element"""
     if lst:
         return lst[0]
