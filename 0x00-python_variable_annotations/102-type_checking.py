9eb34b2c0f354ec9bf577e748871fc2deb990b5b
Drihmia <drihmia.redouane@gmail.com>
2024-06-07 22:25:13 +0100

    task 12, remove __init__.py file (added for testing and pushed by mistake), apparently it misses mypy

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module contains zoom_array as a function"""
+from typing import Tuple, List
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: List = [
+        item for item in lst
+    ]
+    return zoomed_in * factor
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
