5a5efa38545114c3b7d925171a052b7f45c689ae
Drihmia <drihmia.redouane@gmail.com>
2024-06-07 21:49:45 +0100

    task 11

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""task 11"""
+from typing import TypeVar, Mapping, Any
+
+
+T = TypeVar('T')
+# Key = int | float | str | bool | Tuple[int, int]
+
+
+def safely_get_value(dct: Mapping[Any, Any],
+                     key: Any,
+                     default: T | None = None) -> T | Any:
+    """safely_get_value"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
