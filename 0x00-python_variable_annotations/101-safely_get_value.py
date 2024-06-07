5295eb94b70ce1cee9e2166b863db060fbb042c9
Drihmia <drihmia.redouane@gmail.com>
2024-06-07 23:03:22 +0100

    [UPDATE] task 11, use create NoneType using TypeVar

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """task 11"""
-from typing import TypeVar, Mapping, Any, Dict
+from typing import TypeVar, Mapping, Any, Union
 
 
 T = TypeVar('T')
+NoneType = TypeVar('NoneType', bound=None)
 # Key = int | float | str | bool | Tuple[int, int]
 
 
-def safely_get_value(dct: Dict[Any, Any],
+def safely_get_value(dct: Mapping[Any, Any],
                      key: Any,
-                     default: T | None = None) -> T | Any:
+                     default: Union[T, NoneType]) -> Union[Any, T]:
     """safely_get_value"""
     if key in dct:
         return dct[key]
