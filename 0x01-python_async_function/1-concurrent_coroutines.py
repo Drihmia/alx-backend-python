49d96d9ba573a482da800ce33715f3cba8509a41
Drihmia <drihmia.redouane@gmail.com>
2024-06-10 19:15:53 +0100

    test

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Module that contains a async function called wait_random"""
+
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[asyncio.Task]:
+async def wait_n(n: int, max_delay: int) -> List:
     """
     A function that  that takes in 2 int arguments (in this order):
     n and max_delay. You will spawn wait_random n times with the
