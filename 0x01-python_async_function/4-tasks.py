061091bddfa26336e50c65c4c83eaf7fef8c4ed4
Drihmia <drihmia.redouane@gmail.com>
2024-06-10 20:27:26 +0100

    fixed the max delay name and  added python hints

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 '''Module contains task_wait_n as a function'''
 import asyncio
+from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def task_wait_n(n, may_delay):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """A function that alter  from wait_n function"""
-    task = await asyncio.create_task(wait_n(n, may_delay))
+    task = await asyncio.create_task(wait_n(n, max_delay))
     return task
