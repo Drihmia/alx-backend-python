fbdbee6f501a9a93bb2732311b281ae39d3988e3
Drihmia <drihmia.redouane@gmail.com>
2024-06-10 20:13:51 +0100

    return explicitly list of float

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     A function that  that takes in 2 int arguments (in this order):
     n and max_delay. You will spawn wait_random n times with the
