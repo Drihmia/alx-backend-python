2ff7ee912d0fe7847f5b381d14627350c63ed1d2
Drihmia <drihmia.redouane@gmail.com>
2024-06-11 17:14:11 +0100

    test

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """This module contains async_generator as asynchronous generator"""
-from random import uniform
-from asyncio import sleep
+import random
+import asyncio
 from typing import AsyncGenerator
 
 
+# An alternative: AsyncIterator[float] instead of AsyncGenerator[float, None]
 async def async_generator() -> AsyncGenerator[float, None]:
     """
     A coroutine that takes no arguments,
@@ -12,5 +13,5 @@ async def async_generator() -> AsyncGenerator[float, None]:
     then yield a random number between 0 and 10.
     """
     for _ in range(10):
-        await sleep(1)
-        yield uniform(0, 10)
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
