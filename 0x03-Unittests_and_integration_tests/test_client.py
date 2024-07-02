4f39292c4f0ee18d4432fa57bb5954c0f023087f
Drihmia <drihmia.redouane@gmail.com>
2024-07-02 02:26:42 +0100

    feat: Add task 8

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This module tests the client module."""
 from client import GithubOrgClient
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 import unittest
-from unittest.mock import patch, PropertyMock
+from unittest.mock import patch, PropertyMock, Mock
 import utils
 from fixtures import TEST_PAYLOAD
 
@@ -44,7 +44,7 @@ class TestGithubOrgClient(unittest.TestCase):
         with patch.object(GithubOrgClient,
                           '_public_repos_url',
                           return_value="""
-                          https://api.github.com/orgs/google/repos""",
+https://api.github.com/orgs/google/repos""",
                           new_callable=PropertyMock) as r:
             pub_repos = git_client.public_repos()
             expected = TEST_PAYLOAD[0][2]
@@ -62,6 +62,37 @@ class TestGithubOrgClient(unittest.TestCase):
             utils.access_nested_map(
                 repo, ('license', 'key')
             ) == license_key, expected)
+
+
+@parameterized_class((
+    'org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'), [
+        (TEST_PAYLOAD[0][0], TEST_PAYLOAD[0][1],
+         TEST_PAYLOAD[0][2], TEST_PAYLOAD[0][3]),
+    ])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.get_patcher = patch('requests.get')
+        cls.mock_get = cls.get_patcher.start()
+        cls.mock_get.side_effect = cls.mock_request_get
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.get_patcher.stop()
+
+    @classmethod
+    def mock_request_get(cls, url):
+        mock_response = Mock()
+        if url == 'https://api.github.com/orgs/google':
+            mock_response.json.return_value = cls.org_payload
+        elif url == 'https://api.github.com/orgs/google/repos':
+            mock_response.json.return_value = cls.repos_payload
+        return mock_response
+
+    def test_test(self):
+        git_client = GithubOrgClient('google')
+        pub_repos = git_client.public_repos()
+        self.assertEqual(pub_repos, self.expected_repos)
 
 
 if __name__ == '__main__':
