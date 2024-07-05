ea6917dc3f65f73f4624288578e72187bb4c5791
Drihmia <drihmia.redouane@gmail.com>
2024-07-05 22:47:22 +0100

    docs: Add type checking

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,6 +6,7 @@ from unittest.mock import patch, PropertyMock, Mock
 from client import GithubOrgClient
 from fixtures import TEST_PAYLOAD
 import utils
+from typing import Dict
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -16,7 +17,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ('abc', 'https://api.github.com/orgs/abc')
     ])
     @patch('client.get_json')
-    def test_org(self, org, arg, mock_org):
+    def test_org(self, org: str, arg: str, mock_org: Mock) -> None:
         """A test case for org method """
         git_client = GithubOrgClient(org)
         org_resp_mock = git_client.org
@@ -25,7 +26,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
         mock_org.assert_called_once_with(arg)
 
-    def test_public_repos_url(self):
+    def test_public_repos_url(self) -> None:
         """ A test case for _public_repos_url method """
         mock_payload = {"repos_url": "google"}
         with patch.object(GithubOrgClient, 'org',
@@ -37,7 +38,7 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(pub_repo, mock_payload["repos_url"])
 
     @patch('client.get_json', return_value=TEST_PAYLOAD[0][1])
-    def test_public_repos(self, mock_get_jsonn):
+    def test_public_repos(self, mock_get_jsonn: Mock) -> None:
         """ A test case for public_repos method """
         git_client = GithubOrgClient('google')
 
@@ -56,12 +57,13 @@ https://api.github.com/orgs/google/repos""",
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
     ])
-    def test_has_license(self, repo, license_key, expected):
+    def test_has_license(self, repo: Dict[str, Dict], license_key: str,
+                         expected: bool) -> None:
         """ A test case for has_license method """
+        git_client = GithubOrgClient('google')
         self.assertEqual(
-            utils.access_nested_map(
-                repo, ('license', 'key')
-            ) == license_key, expected)
+            git_client.has_license(
+                repo, license_key), expected)
 
 
 @parameterized_class((
@@ -70,18 +72,21 @@ https://api.github.com/orgs/google/repos""",
          TEST_PAYLOAD[0][2], TEST_PAYLOAD[0][3]),
     ])
 class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ A class for integration test cases for GithubOrgClient class """
     @classmethod
-    def setUpClass(cls):
+    def setUpClass(cls) -> None:
+        """ A class method to set up the mock get """
         cls.get_patcher = patch('requests.get')
         cls.mock_get = cls.get_patcher.start()
         cls.mock_get.side_effect = cls.mock_request_get
 
     @classmethod
-    def tearDownClass(cls):
+    def tearDownClass(cls) -> None:
+        """ A class method to stop the patcher """
         cls.get_patcher.stop()
 
     @classmethod
-    def mock_request_get(cls, url):
+    def mock_request_get(cls, url: str) -> Mock:
         """ A class method to apply side effect on the mock get """
         mock_response = Mock()
         if url == 'https://api.github.com/orgs/google':
@@ -90,18 +95,14 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             mock_response.json.return_value = cls.repos_payload
         return mock_response
 
-    def test_public_repos(self):
+    def test_public_repos(self) -> None:
         """ A test for public_repos method """
         git_client = GithubOrgClient('google')
         pub_repos = git_client.public_repos()
         self.assertEqual(pub_repos, self.expected_repos)
 
-    def test_public_repos_with_license(self):
+    def test_public_repos_with_license(self) -> None:
         """ A test for has_license method """
         git_client = GithubOrgClient('google')
         app_repos = git_client.public_repos('apache-2.0')
         self.assertEqual(app_repos, self.apache2_repos)
-
-
-if __name__ == '__main__':
-    unittest.main()
